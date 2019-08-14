df085f5c67e387b592a450378dfd5044fa575e19
Natanael <natan-fernandes1@hotmail.com>
2019-08-14 01:16:57 -0300

    oop optimization in code

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
+import numpy as np  
 pygame.init()
 
-windowW = 1000
-windowH = 600
 clock = pygame.time.Clock()
-window = pygame.display.set_mode((windowW,windowH))
-pygame.display.set_caption("Car crash")
+
+class Game(object):
+    def __init__(self,width,height,title):
+        self.window = pygame.display.set_mode((width,height))
+        self.width = width
+        self.height = height
+        self.title = title
+        self.caption = pygame.display.set_caption(title)
 
 class Car(object):
-    def __init__(self,x,y,width,height):
+    def __init__(self,x,y,width,height,velocity):
         self.x = x
         self.y = y
         self.width = width
@@ -17,10 +22,14 @@ class Car(object):
         self.left = False
         self.right = False
         self.bottom = False
+        self.vel = velocity
 
-# start at bottom middle of window
-x = windowH-100
-y = windowW/2
+class Trash(object):
+    def __init__(self,width,height):
+        self.x = np.random.randint(game.width - 100)
+        self.y = np.random.randint(game.height - 100)
+        self.width = width
+        self.height = height
 
 # images of cars curve
 curveRight = pygame.image.load('r3.png')
@@ -28,44 +37,44 @@ curveLeft = pygame.image.load('l3.png')
 curveBottom = pygame.image.load('b3.png')
 carNormal = pygame.image.load("car.png")
 
-width = 64
-height = 64
-vel = 20
 game = True
-
-left = False
-right = False
-bottom = False
 
 def create_car(img):
     car_surface = pygame.Surface((64,64))  
-    car_surface.set_alpha(0)  
+    car_surface.set_alpha(255)  
     car_surface.fill((255,255,255))
-    window.blit(car_surface, (x,y))
-    window.blit(img, (x,y))
 
+    game.window.blit(car_surface, (car.x,car.y))
+    game.window.blit(img, (car.x,car.y))
 
 def create_trash():
-    trash = pygame.image.load("trash.png")
+    trashImage = pygame.image.load("trash.png")
+
     trash_surface = pygame.Surface((64,64))  
     trash_surface.set_alpha(0)  
     trash_surface.fill((255,255,255))
-    window.blit(trash_surface, (100,100))
-    window.blit(trash, (100,100))
+    
+    game.window.blit(trash_surface, (trash.x,trash.y))
+    game.window.blit(trashImage, (trash.x,trash.y))
+    pygame.display.update()
 
 def create_bg():
     bg = pygame.image.load("bg.png")
-    window.blit(bg, (0, 0))
+    game.window.blit(bg, (0, 0))
 
-    if left:
+    if car.left:
         create_car(curveLeft)
-    elif right:
+    elif car.right:
         create_car(curveRight)
-    elif bottom:
+    elif car.bottom:
         create_car(curveBottom)
     else:
         create_car(carNormal)
     pygame.display.update()
+
+game = Game(1000,600,"Car crash")
+car = Car((game.height-100),(game.width/2),64,64,20)
+trash = Trash(64,64)
 
 while game:
     clock.tick(9)
@@ -77,39 +86,32 @@ while game:
 
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_LEFT] and x > vel:
-        x = x-vel
-        left = True
-        right = False
-        bottom = False
-       
-        print(x)
-
-    if keys[pygame.K_RIGHT] and x < (windowW-width):
-        x = x+vel
-        left = False
-        right = True
-        bottom = False
+    if keys[pygame.K_LEFT] and car.x > car.vel:
+        car.x = car.x - car.vel
+        car.left = True
+        car.right = False
+        car.bottom = False
         
-        print(x)
+    if keys[pygame.K_RIGHT] and car.x < (game.width - car.width):
+        car.x = car.x + car.vel
+        car.left = False
+        car.right = True
+        car.bottom = False
 
-    if keys[pygame.K_UP] and y > 0:
-        y = y-vel
-        left = False
-        right = False
-        bottom = False
-        
-        print(y)
+    if keys[pygame.K_UP] and car.y > 0:
+        car.y = car.y - car.vel
+        car.left = False
+        car.right = False
+        car.bottom = False
 
-    if keys[pygame.K_DOWN] and y < (windowH-height-20):
-        y= y+vel
-        left = False
-        right = False
-        bottom = True
-        
+    if keys[pygame.K_DOWN] and car.y < (game.height - car.height-20):
+        car.y = car.y + car.vel
+        car.left = False
+        car.right = False
+        car.bottom = True     
 
     create_bg()
-
+    create_trash()
 
 
 pygame.quit()
